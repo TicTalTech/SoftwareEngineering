a55e730a451c38a48215a889312c1f1a3528be9f
Tal <tal.schul@campus.technion.ac.il>
2023-05-10 17:29:31 +0300

    new solver

--- src/HW1/Solver.java
+++ src/HW1/Solver.java
@@ -1,4 +1,6 @@
 package HW1;
 
+// https://www.cs.princeton.edu/courses/archive/spring21/cos226/assignments/8puzzle/specification.php#:~:text=Thus%2C%20if%20a%20board%20has,inversions%2C%20then%20it%20is%20solvable.
+// https://www.kopf.com.br/kaplof/how-to-solve-any-slide-puzzle-regardless-of-its-size/
 public class Solver {
 }
